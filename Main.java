d7cc6a65be101b0ac300eaf476e784f088fcf652
arha83 <amirrhosseinali@gmail.com>
2024-03-26 20:38:05 +0330

    added screenshots and snippets

--- Main.java
+++ Main.java
@@ -1,21 +1,5 @@
-import java.awt.Color;
-
 public class Main{
     public static void main(String args[]){
-        SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");
-        
-        int x= 400, y= 300;
-        while (true) {
-            sgw.clear();
-            sgw.drawSimpleCircle(x, y, 30, new Color(0, 255, 255));
-
-            delay(1);
-        }
-    }
-
-    private static void delay(int time){
-        try{
-            Thread.sleep(50);
-        } catch(InterruptedException e){}
+        SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");   
     }
 }
