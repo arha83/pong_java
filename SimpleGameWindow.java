63cd85b4a940bbe955c2f3565e6fbd6ca72a162d
arha83 <amirrhosseinali@gmail.com>
2024-03-26 11:10:54 +0330

    Code Comments: Documented TA's code effectively with meaningful comments to improve readability and facilitate future maintenance and debugging.

--- SimpleGameWindow.java
+++ SimpleGameWindow.java
@@ -1,3 +1,4 @@
+// importing packages (aka libraries)
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -8,30 +9,36 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.Queue;
-
+// importing other packages (other libraries)
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-
+// window class
+// develop the project by changing this class
 public class SimpleGameWindow {
+    // window related objects
     private JFrame frame;
     private JPanel panel;
     private Image image;
-
+    // window variables
     private int width;
     private int height;
-
+    // a que for storing pressed keys
     private Queue<Character> keyQueue;
-
-    public SimpleGameWindow(int width, int height, String title)
-    {
+    // class constructor
+    // this method is run when a new instance of this class is created
+    // parameters:
+    //   width      -> desired window width in pixels
+    //   height     -> desired window height in pixels
+    //   title      -> window title that appears on top bar
+    public SimpleGameWindow(int width, int height, String title){
+        // setting size variables for futures uses
         this.height = height;
         this.width = width;
-
+        // initializing the que
         keyQueue = new LinkedList<>();
-
+        // some giberrish about initializing the window and frame and blah blah blah....
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        
         frame = new JFrame(title);
         panel = new JPanel(true) {
             @Override
@@ -40,7 +47,6 @@ public class SimpleGameWindow {
                 g.drawImage(image, 0, 0, null); //TODO: Attend null observer later
             }
         };
-
         frame.setSize(width, height);
         panel.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -49,7 +55,7 @@ public class SimpleGameWindow {
         frame.setFocusable(false);
         panel.setFocusable(true);
         panel.requestFocus();
-
+        // other gibberish about setting key listener and etc...
         KeyListener keyListener = new KeyListener() {
             @Override
             public void keyPressed(KeyEvent arg0) {
@@ -64,9 +70,8 @@ public class SimpleGameWindow {
                 keyQueue.add(ke.getKeyChar());
             }
         };
-
         panel.addKeyListener(keyListener);
-        
+        // showing the window
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -74,7 +79,12 @@ public class SimpleGameWindow {
             }
         });
     }
-
+    // this method draws a complex circle
+    // parameters:
+    //   x           -> circle center x
+    //   y           -> circle center y
+    //   r           -> circle radius
+    //   colour      -> circle color object (not colour 😒)
     public void drawSimpleCircle(int x, int y, int r, Color colour)
     {
         SwingUtilities.invokeLater(new Runnable() {
@@ -87,7 +97,7 @@ public class SimpleGameWindow {
             }
         });
     }
-
+    // this method clears the frame
     public void clear() {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -98,7 +108,7 @@ public class SimpleGameWindow {
             }
         });
     }
-
+    // this method gets a key from user (like getch() in c++ but worse)
     public Character getKey() {
         if (!keyQueue.isEmpty())
             return keyQueue.poll();
