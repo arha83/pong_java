74cd0f9c0465f45c08bab2c0ac33b3da78ac8638
arha83 <amirrhosseinali@gmail.com>
2024-03-26 21:47:40 +0330

    added screenshots and snippets

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.awt.Color;
+
 public class Main{
     public static void main(String args[]){
         SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");
 
-        sgw.drawSimpleCircle(400, 300, 0, null);
+        sgw.drawSimpleCircle(200, 100, 40, new Color(0, 255, 255));
     }
 }
