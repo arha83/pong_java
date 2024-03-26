28aa47e96107d03972b87e4174f750cba1f2fc73
arha83 <amirrhosseinali@gmail.com>
2024-03-26 22:00:28 +0330

    added screenshots and snippets

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main{
         SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");
 
         sgw.drawSimpleCircle(200, 100, 40, new Color(0, 255, 255));
+        sgw.clear();
+        sgw.drawSimpleCircle(400, 100, 30, new Color(255, 0, 255));
     }
 }
