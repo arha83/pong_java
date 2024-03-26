bd5c437968ddac84e81ae53af8ded605b6dad661
arha83 <amirrhosseinali@gmail.com>
2024-03-26 20:59:58 +0330

    added an image

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String args[]){
-        SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");   
+        SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");
+
+        sgw.drawSimpleCircle(400, 300, 0, null);
     }
 }
