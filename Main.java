c65bce4eac0e07c94a1ccfe8419113e847385372
arha83 <amirrhosseinali@gmail.com>
2024-03-26 22:47:50 +0330

    added a snippet

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
-import java.awt.Color;
-
 public class Main{
     public static void main(String args[]){
         SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");
 
-        sgw.drawSimpleCircle(200, 100, 40, new Color(0, 255, 255));
-        sgw.clear();
-        sgw.drawSimpleCircle(400, 100, 30, new Color(255, 0, 255));
+        Character input= null;
+        while(input == null){
+            input= sgw.getKey();
+            // wait a bit
+            try{
+                Thread.sleep(1);
+            } catch(InterruptedException e){};
+        }
+        // doing whatever I want
+        System.out.println(input);
     }
 }
