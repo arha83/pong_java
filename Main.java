e9300e36eb9e5ed8d09922f015561aa9c0fd0ff0
arha83 <amirrhosseinali@gmail.com>
2024-04-03 14:12:16 +0330

    Added audio playing support

--- Main.java
+++ Main.java
@@ -1,23 +1,7 @@
 public class Main{
     public static void main(String args[]){
-        SimpleGameWindow sgw= new SimpleGameWindow(800, 600, "woooooop");
-        // sprite and hitbox
-        Sprite spr= new Sprite("./assets/animations/troll", 1, 400, 300);
-        Hitbox hb= new Hitbox(350, 250, 100, 100);
-        // game object with initial velocity of (4, 3)
-        GameObject object= new GameObject(spr, hb, 400, 300, 4, -10, 0, 0.1f);
-        object.setXY(0, 600);
-        // updating the object position in a loop and drawing it
-        while(true){
-            // draw
-            sgw.clear();
-            sgw.drawGameObject(object, true);
-            // update
-            object.update();
-            // small delay
-            try{
-                Thread.sleep(10);
-            } catch(InterruptedException e){}
-        }
+        AudioPlayer ap1= new AudioPlayer("./assets/sfx/demo.wav");
+        ap1.initAudioStream();
+        ap1.loop();
     }
 }
